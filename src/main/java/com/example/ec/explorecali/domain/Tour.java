d443f92caaabda98fad2020f38da59e97665b1f5
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-10-07 06:03:02 +0200

    added tour enum domain model -defined states and behaviours of the model

---
+++ src/main/java/com/example/ec/explorecali/domain/Tour.java
@@ -0,0 +1,50 @@
+package com.example.ec.explorecali.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Tour implements Serializable {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    @Column(length = 2000)
+    private String blurb;
+
+    @Column
+    private Integer price;
+
+    @Column
+    private String duration;
+
+    @Column(length = 2000)
+    private String bullets;
+
+    @Column
+    private String keywords;
+
+
+    @ManyToOne
+    private TourPackage tourPackage;
+
+    @Column
+    private Difficulty difficulty;
+
+    @Column
+    private Region region;
+}
