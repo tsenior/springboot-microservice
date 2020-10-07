994fbb613ca76878648628194229a6894e16a99f
f5295785 <tinyiko.chauke1@fnb.co.za>
2020-10-07 06:25:53 +0200

    added TourPackageRepository to manage Tour Entity -extends CrudRepository to CRUD default functions

---
+++ src/main/java/com/example/ec/explorecali/repository/TourPackageRepository.java
@@ -0,0 +1,7 @@
+package com.example.ec.explorecali.repository;
+
+import com.example.ec.explorecali.domain.TourPackage;
+import org.springframework.data.repository.CrudRepository;
+
+public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
+}
